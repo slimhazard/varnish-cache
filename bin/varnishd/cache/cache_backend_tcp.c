86439332900a122db82a2a16be035da74b38d7f6
Geoff Simmons <geoff@uplex.de>
2017-08-24 11:46:11 +0200

    Add VSA_Malloc_UDS(), and use it for backends and TCP pools.

--- bin/varnishd/cache/cache_backend_tcp.c
+++ bin/varnishd/cache/cache_backend_tcp.c
@@ -138,8 +138,6 @@ VBT_Ref(const struct suckaddr *ip4, const struct suckaddr *ip6,
 	const struct suckaddr *uds)
 {
 	struct tcp_pool *tp;
-	const void *uds_addr;
-	socklen_t sl;
 
 	assert(uds != NULL || ip4 != NULL || ip6 != NULL);
 	Lck_Lock(&pools_mtx);
@@ -158,13 +156,9 @@ VBT_Ref(const struct suckaddr *ip4, const struct suckaddr *ip6,
 	ALLOC_OBJ(tp, TCP_POOL_MAGIC);
 	AN(tp);
 	if (uds != NULL) {
-		uds_addr = VSA_Get_Sockaddr(uds, &sl);
-		AN(uds_addr);
-		tp->uds_sockaddr = malloc(sl);
-		AN(tp->uds_sockaddr);
-		memcpy(tp->uds_sockaddr, uds_addr, sl);
-		tp->uds = VSA_Malloc(tp->uds_sockaddr, 0, tp->uds_sockaddr);
+		tp->uds = VSA_Malloc_UDS(uds, &tp->uds_sockaddr);
 		AN(tp->uds);
+		AN(tp->uds_sockaddr);
 	}
 	else {
 		if (ip4 != NULL)
